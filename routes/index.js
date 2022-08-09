b17cc9e1b7fed78281857023bd01f72f628a42ec
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-08-09 11:53:43 -0400

    displaying all messages on the index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 router.get('/', (req, res) => {
-  res.send('Message List');
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
 module.exports = router;
