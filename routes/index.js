44f9a5e6632be60adfec443ef393c3b831614f06
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2022-08-10 21:12:47 -0400

    can delete messages

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,16 @@ router.get('/', async (req, res) => {
   res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.delete('/delete/:id', async (req, res) => {
+  let message;
+
+  try {
+    message = await Message.findById(req.params.id);
+    await message.remove();
+    res.redirect('/');
+  } catch (error) {
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
